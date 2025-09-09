4d5e9a3e90e22af312f43d1ed3456f1ed40dc088
drknvpn <149516500+drknvpn@users.noreply.github.com>
2025-09-09 19:34:33 +0500

    Файлики проекта

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LandingPage } from './components/LandingPage';
+import './styles/animations.css';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black">
+      <LandingPage />
+    </div>
+  );
+}
